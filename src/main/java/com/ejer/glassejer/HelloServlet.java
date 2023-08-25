88cb665d5553ed3862706cfcbf3dd15693cd740f
Orland H <24heyerunsu@gmail.com>
2023-08-25 12:50:01 -0500

    Modelo

--- src/main/java/com/ejer/glassejer/HelloServlet.java
+++ src/main/java/com/ejer/glassejer/HelloServlet.java
@@ -1,66 +1,54 @@
 package com.ejer.glassejer;
 
 import java.io.*;
-
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
-
-
 import java.sql.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
-    public String nombre, fechanac, signoz;
-    private int codigo, cedula;
+
     static final String DB_URL = "jdbc:mysql://localhost/POOPRUEBA2";
     static final String USER = "root";
     static final String PASS = "root_bas3";
 
     @Override
     public void init() {
-        message = "Aqui se mostrara los datos de la base:";
-        nombre="heyer tinoco";
-        try(Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);){
-            String SQL_Query_select = "SELECT * FROM est_prueba2 WHERE nombre like ?";
-            try(PreparedStatement pstm = conn.prepareStatement(SQL_Query_select)){
-                pstm.setString(1,nombre);
-                try (ResultSet rs = pstm.executeQuery()){
-                    if (rs.next()){
-                        codigo= rs.getInt("codigo_est");
-                        cedula = rs.getInt("cedula");
-                        fechanac= rs.getString("fecha_nac");
-                        signoz=rs.getString("signo");
-
-                    } else {
-
-                    }
-                }
-            }
-        } catch (SQLException ex){
-            ex.printStackTrace();
-            nombre="Holi";
-            fechanac="No se esta conectandoaaooo";
-        }
+        message = "Aquí se mostrarán los datos de la base:";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
-        // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
-        out.println("<h1>" + "Código de Estudiante:" + "</h1>");
-        out.println("<p>"+codigo+"</p>");
-        out.println("<h1>" + "Cedula estudiante:" + "</h1>");
-        out.println("<p>"+cedula+"</p>");
-        out.println("<h1>" + "Nombre:" + "</h1>");
-        out.println("<p>"+nombre+"</p>");
-        out.println("<h1>" + "Fecha de Nacimiento:" + "</h1>");
-        out.println("<p>"+fechanac+"</p>");
-        out.println("<h1>" + "Signo Zodiacal:" + "</h1>");
-        out.println("<p>"+signoz+"</p>");
+
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM est_prueba2")) {
+
+            out.println("<table border='1'>");
+            out.println("<tr><th>Código</th><th>Cédula</th><th>Nombre</th><th>Fecha de Nacimiento</th><th>Signo Zodiacal</th></tr>");
+
+            while (rs.next()) {
+                int codigo = rs.getInt("codigo_est");
+                int cedula = rs.getInt("cedula");
+                String nombre = rs.getString("nombre");
+                String fechanac = rs.getString("fecha_nac");
+                String signoz = rs.getString("signo");
+
+                out.println("<tr><td>" + codigo + "</td><td>" + cedula + "</td><td>" + nombre + "</td><td>" + fechanac + "</td><td>" + signoz + "</td></tr>");
+            }
+
+            out.println("</table>");
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            out.println("Error al acceder a la base de datos.");
+        }
+
         out.println("</body></html>");
     }
 
